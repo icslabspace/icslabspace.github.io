f8dd119288261c77c53ffc9ac497c7ec0a3a0272
georgi-mania <georgiana.mania@gmail.com>
2016-07-22 13:39:46 +0300

    add site content

---
+++ assets/library/das-site.js
@@ -0,0 +1,37 @@
+$(document)
+    .ready(function() {
+
+      // fix menu when passed
+      $('.masthead')
+        .visibility({
+          once: false,
+          onBottomPassed: function() {
+            $('.fixed.menu').transition('fade in');
+          },
+          onBottomPassedReverse: function() {
+            $('.fixed.menu').transition('fade out');
+          }
+        });
+      
+
+// create sidebar and attach to menu open
+$('.ui.sidebar')
+	.sidebar('attach events', '.toc.item');
+      
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+});
